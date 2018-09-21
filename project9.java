5ab0aac5c69db49ae0638779af46f59ee739a6df
bishal135 <41116592+bishal135@users.noreply.github.com>
2018-09-21 08:14:51 +0530

    Update project9.java

--- project9.java
+++ project9.java
@@ -5,12 +5,12 @@ public class project9 {
 	public static void main(String[] args) {
 		System.out.println(new project9().run());
 	}
-	public String run() {
+	public int run() {
 		for (int a = 1; a < 1000; a++) {
 			for (int b = a + 1; b < 1000; b++) {
 				int c = 1000 - a - b;
 				if (a * a + b * b == c * c)  // Note: This implies b < c
-					return Integer.toString(a * b * c);
+					return (a * b * c);
 			}
 		}
 		throw new AssertionError("Not found");
